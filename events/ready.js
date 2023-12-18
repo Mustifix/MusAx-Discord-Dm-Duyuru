05dfa3f8968bc33ff40288e4ef61586cc3189c77
Mustifix <131684539+Mustifix@users.noreply.github.com>
2023-12-18 17:33:33 +0300

    Musax-Dm-Duyuru
    Discord V14 DM gelişmiş duyuru botu!

---
+++ events/ready.js
@@ -0,0 +1,22 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+
+module.exports = client => {
+  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("dnd");
+  //idle = boşta
+  //dnd = rahatsız etmeyin
+  //online = çevrimiçi
+  console.log(`                                                                                                                                                                     `)
+  client.user.setActivity(`YouTube: @layrixx | @axelbynow ' `, { type: "WATCHİNG"});
+  //LISTENING = DİNLİYOR
+  //WATCHING = İZLİYOR
+  //PLAYING = OYNUYOR 
+  console.log("Desinged/Developed By: Mustifix - Axel")
+  console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
